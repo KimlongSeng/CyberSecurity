c6f042448906e4b9540239da07c3617b0ceb12fe
KimlongSeng <kimlong.seng1998@gmail.com>
2020-03-08 00:50:12 -0500

    3/7/2020

--- firewall.sh
+++ firewall.sh
@@ -1,14 +1,14 @@
 start1()
 {
 printf "\n******Hello Welcome to The Help Menu*****\n
-1.Check Iptable Status
+1. Check list of Rules
 2.Add Firewall Rule
-3.Delete All Firewall Rule
+3.Delete Firewall Rule
 4.Modify Firewall Rule
 5.Save
 6.Exit\n
 Please Type In a Number to choose your option\n
-"
+    "
 function
 }
 
@@ -17,18 +17,106 @@ function(){
 read -p "Type In a Number :" num
 printf "\n"
 echo "You pick option :" $num
+
+if [ $num -eq 4 ]
+then 
+
+echo " 
+1) Insert Rule
+2) Replace Rule
+3) Go to Main Menu\n
+"
+read num4
+case $num4 in
+ 1) echo "\nplease Enter the Chain(INPUT,FORWARD,OUTPUT) and Hit Enter key \n"
+    read -p " Chain :" chain1
+    echo "\nplease Enter the Rule Number and Hit Enter key \n"
+    read -p " Rule :" rules1
+    echo "\nplease Enter DROP OR ACCEPT and Hit Enter key \n"
+    read -p "DROP OR ACCEPT :" DorP
+    echo " the result : sudo iptables -I $chain1 $rules1 -j $DorP"
+    sudo iptables -I $chain1 $rules1 -j $DorP;;
+
+ 2) echo "\nplease Enter the Chain(INPUT,FORWARD,OUTPUT) and Hit Enter key \n"
+    read -p " Chain :" chain1
+    echo "\nplease Enter the Rule Number and Hit Enter key \n"
+    read -p " Rule :" rules1
+    echo "\nplease Enter the Rule Number and Hit Enter key (i.e 5.158.238.32)\n"
+    read -p " New Rule Source :" sources
+    echo "\nplease Enter DROP OR ACCEPT and Hit Enter key \n"
+    read -p "DROP OR ACCEPT :" DorP
+    echo "the result : sudo ipatble -R $chain1 $rules1 -s $sources -j $DorP"
+    sudo iptables -R $chain1 $rules1 -s $sources -j $DorP;;
+ 3) start1;;
+esac
+
+fi 
+
+if [ $num -eq 3 ]
+then 
+  echo  "\n*************************************************\n"
+  echo  "
+    1. Delete Rule by Chain and Number
+    2. Flush By Chain
+    3. Flush All Chains (Delete All)
+    4. Go to Main Menu\n
+  "
+  read num1
+  case $num1 in
+
+  1) echo " 
+  please enter the chain
+  1. INPUT
+  2. OUTPUT
+  3. Forward
+  4. Go to Main Menu\n"
+    read num1a
+    case $num1a in
+    1) echo " enter number of rule"
+    read numrule
+    sudo iptables -D $num1a $numrule;;
+    2) echo " enter number of rule"
+    read numrule
+    sudo iptables -D $num1a $numrule;;
+    3) echo  " enter number of rule"
+    read numrule
+    sudo iptables -D $num1a $numrule;;
+    4) start1;;
+    esac ;;
+  2) echo " 
+      please enter the chain
+    1. INPUT
+    2. OUTPUT
+    3. Forward
+    4. Go to Main Menu\n"
+    read num1b
+    case $num1b in
+    1) sudo iptables -F INPUT;;
+    2) sudo iptables -F OUTPUT;;
+    3)sudo iptables -F FORWARD;;
+    4) start1;;
+    esac ;;
+  3) sudo iptables -F;;
+  4) start1;;
+esac
+fi
+
+
+
 if [ $num -eq 1 ]
 then
-    echo -e "***************************************************\n"
-    sudo iptables -L
-    echo  -e "\n"
-    sudo iptables -S
-    echo -e "\n*************************************************\n"
+    echo  "***************************************************\n"
+    sudo iptables -L --line-numbers
+    echo  "\n*************************************************\n
+    ******Press enter to return to menu
+    "
+    read temp
+    start1
 fi 
 if [ $num -eq 2 ]
 then
 
-    echo -e " ***** Add Firewall Rule *****\n
+    echo  " ***** Add Firewall Rule *****\n
     Please Choose a Chain of Fliter Table?\n
     1. INPUT
     2. OUTPUT
@@ -41,10 +129,10 @@ then
     2) chain="OUTPUT" ;;
     3) chain="FORWARD" ;;
     4) start1 ;;
-    *) echo -e "Please 1, 2, 3, or 4 !"
+    *) echo  "Please 1, 2, 3, or 4!"
     esac
 
-    echo -e "
+    echo  "
     1. Firewall with Single Source IP\n
     2. Firewall with Source Subnet
     3. Firewall with for All Source Networks
@@ -52,16 +140,16 @@ then
     
     read option_IP
     case $option_IP in
-    1) echo -e "\nPlease Enter the IP Address of the Source"
+    1) echo  "\nPlease Enter the IP Address of the Source"
     read ip_source ;;
-    2) echo -e "\nPlease Enter the Source Subnet (Example 111.111.11.1/11)"
+    2) echo  "\nPlease Enter the Source Subnet (Example 111.111.11.1/11)"
     read ip_source ;;
     3) ip_source="0/0" ;;
     4)start1;;
-   *) echo -e "Please 1, 2, 3, or 4 !"
+   *) echo  "Please 1, 2, 3, or 4 !"
     esac
 
-    echo -e " Getting IP Address\n
+    echo  " Getting IP Address\n
     1. Using Single Destination IP
     2. Using Destination Subnet
     3. Using for All Destination Networks
@@ -69,15 +157,15 @@ then
 
     read opt_ip
     case $opt_ip in
-    1) echo -e "\nPlease Enter the IP Address of the Destination"
+    1) echo  "\nPlease Enter the IP Address of the Destination"
        read ip_dest ;;
-    2) echo -e "\nPlease Enter the Destination Subnet (example 111.111.11.1/11)"
+    2) echo  "\nPlease Enter the Destination Subnet (example 111.111.11.1/11)"
        read ip_dest ;;
     3) ip_dest="0/0" ;;
     4) start1;;
-    *) echo -e "Please 1, 2, or 3!!!"
+    *) echo  "Please 1, 2, or 3!!!"
        esac
-    echo -e " Getting Protocal \n
+    echo " Getting Protocal \n
        1. Block All Traffic of TCP
        2. Block Specific TCP Service
        3. Block Specific Port
@@ -87,16 +175,16 @@ then
     read proto_ch
     case $proto_ch in
         1) proto=TCP ;;
-        2) echo -e "Enter the TCP Service Name: (CAPITAL LETTERS!!!)"
+        2) echo  "Enter the TCP Service Name: (CAPITAL LETTERS!!!)"
         read proto ;;
-        3) echo -e "Enter the Port Name: (CAPITAL LETTERS!!!)" 
+        3) echo  "Enter the Port Name: (CAPITAL LETTERS!!!)" 
         read proto ;;
         4) proto="NULL" ;;
         5) start1 ;;
-        *) echo -e "Please 1, 2, 3, 4, or 5!"
+        *) echo  "Please 1, 2, 3, 4, or 5!"
     esac
 
-    echo -e "Rule?\n
+    echo  "Rule?\n
        1. Accept the Packet\n
        2. Reject the Packet
        3. Drop the Packet
@@ -109,20 +197,20 @@ then
         3) rule="DROP" ;;
         4) rule="LOG" ;;
         5) start1 ;;
-        *) echo -e "Please 1, 2, 3, 4, or 5!"
+        *) echo  "Please 1, 2, 3, 4, or 5!"
        esac
 
-    echo -e "\n\tPress Enter key to Generate the Complete Rule!!!"
+    echo  "\n\tPress Enter key to Generate the Complete Rule!!!"
     read temp
-    echo -e "The Generated Rule is \n"
+    echo  "The Generated Rule is \n"
     if [ $proto == "NULL" ]; then
-    echo -e "\n sudo iptables -A $chain -s $ip_source -d $ip_dest -j $rule\n"
+    echo  "\n sudo iptables -A $chain -s $ip_source -d $ip_dest -j $rule\n"
     gen=1
     else
-    echo -e "\n sudo iptables -A $chain -s $ip_source -d $ip_dest -p $proto -j $rule\n"
+    echo  "\n sudo iptables -A $chain -s $ip_source -d $ip_dest -p $proto -j $rule\n"
     gen=2
     fi 
-    echo -e "\n\tDo you want to Enter the Above rule to the IPTABLES? Yes=1 , No=2"
+    echo  "\n\tDo you want to Enter the Above rule to the IPTABLES? Yes=1 , No=2"
     read ans
     if [ $ans == 1 ] && [ $gen == 1 ]; then
     sudo iptables -A $chain -s $ip_source -d $ip_dest -j $rule
@@ -135,20 +223,12 @@ fi
 fi
 fi
 fi
-if [ $num -eq 3 ]
-then 
-  echo -e "\n*************************************************\n"
-    sudo iptables -F 
-    sudo iptables -t nat -F
-    sudo iptables -t mangle -F
-    sudo iptables -X
-  echo -e "\n*************************************************\n"
-fi
+
 
 if [ $num -eq 5 ]
 then
     sudo iptables-save
-    echo -e "
+    echo  "
     *****************all file save******************
     \nPress Enter To Go To Main Menu
     "
@@ -156,8 +236,17 @@ then
     start1
 fi
 if [ $num -eq 6 ]
-then 
+then
+echo "\n\t******Exiting The program \n 
+ ********** HAVE A GOOD DAY **********\n" 
 exit 0
+
+fi
+
+if [ $num -le 0 ] || [ $num -ge 7 ]
+then
+echo "please enter the write option i.e 1, 2, 3, 4, 5, 6"
+start1
 fi
 
 }
